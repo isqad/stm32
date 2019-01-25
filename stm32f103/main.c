497dda8f63ab0c6369adea325e59d5e36fc64070
Andrew Shalaev <andy@Andrews-MacBook-Pro.local>
2019-01-25 08:36:13 +0500

    feature: blink on TIM2 IRQ

--- stm32f103/main.c
+++ stm32f103/main.c
@@ -1,7 +1,7 @@
 #define STM32F10X_MD
 #include "stm32f10x.h"
 
-void delay(void);
+uint32_t counter;
 
 int main()
 {
@@ -26,8 +26,6 @@ int main()
     //RCC->CFGR |= RCC_CFGR_SW_HSE;
     RCC->CFGR |= RCC_CFGR_SW_PLL;
 
-    RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
-
     RCC->CR |= RCC_CR_PLLON;
     // wait for ready PLL
     while (!(RCC->CR | RCC_CR_PLLRDY))
@@ -37,32 +35,44 @@ int main()
     // Enable clock for PC port and timer
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
     // Clock 24Mhz for TIM6
-    RCC->APB1ENR |= RCC_APB2ENR_TIM6EN;
-    RCC->APB1RSTR |= RCC_APB1RSTR_TIM6RST;
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+    // enable clock for APB1
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    //RCC->APB1RSTR |= RCC_APB1RSTR_TIM6RST;
 
     // GPIO configuration
     GPIOC->CRH |= GPIO_CRH_MODE13_1; // MODE13[1:0] bits (01) - max speed 10Mhz
     GPIOC->CRH &= ~GPIO_CRH_CNF13; // Reset CNF13[1:0] bits (00) - push pull mode
 
-    /**
+    // prescaler 24000 - 1 = 1ms
+    TIM2->PSC = (uint32_t)(24000 - 1);
+    TIM2->ARR = (uint32_t)(2000 - 1);
+    TIM2->DIER |= TIM_DIER_UIE;
+    TIM2->CR1 |= TIM_CR1_CEN | TIM_CR1_ARPE;
+
+    NVIC_EnableIRQ(TIM2_IRQn);
+
+    GPIOC->BSRR |= GPIO_BSRR_BS13;
+    counter = 1;
+    
     while (1)
     {
-      GPIOC->BSRR |= GPIO_BSRR_BS13;
-      delay();
-      delay();
-      delay();
-      GPIOC->BSRR |= GPIO_BSRR_BR13;
-      delay();
     }
-    **/
-    NVIC_EnableIRQ(TIM6_DAC_IRQn);
+    
+    
     return 0;
 }
 
-void delay(void)
+void TIM2_IRQHandler(void) 
 {
-  for (uint32_t i = 0; i < 80000; i++)
+  TIM2->SR &= ~TIM_SR_UIF;
+
+  if (counter == 1)
   {
-    asm("NOP");
+    GPIOC->BSRR |= GPIO_BSRR_BR13;
+    counter = 0;
+  } else {
+    GPIOC->BSRR |= GPIO_BSRR_BS13;
+    counter = 1;
   }
 }
